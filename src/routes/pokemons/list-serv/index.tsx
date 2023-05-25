367bfa32c34b3abe0e67f3d44b3dfc62909b1800
vengardus <ismytv@gmail.com>
2023-05-25 17:33:27 -0500

    feat:pokemons/list-serv:fetch

--- src/routes/pokemons/list-serv/index.tsx
+++ src/routes/pokemons/list-serv/index.tsx
@@ -1,7 +1,35 @@
 import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { routeLoader$ } from '@builder.io/qwik-city';
+
+const URL = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+
+export const usePokemonList = routeLoader$( async() => {
+  const resp = await fetch(URL)
+  const data = await resp.json()
+  
+  return data
+})
 
 export default component$(() => {
+  const pokemonList = usePokemonList().value.results
+
+  console.log(pokemonList)
+
   return (
-    <>List-serv</>
+    <div class='flex flex-col'>
+
+
+    </div>
   )
 });
+
+export const head: DocumentHead = {
+  title: 'Pokemon List - Server',
+  // meta: [
+  //   {
+  //     name: 'qwik-proy01',
+  //     content: 'this is my first app in qwik',
+  //   },
+  // ],
+};
